e6a37ef46d1e443f286f4f288304396176a28f89
Nath Paiva <hello@nathpaiva.com.br>
2023-08-18 14:09:51 -0700

    chore: adds front-end config

--- packages/client/.eslintrc.cjs
+++ packages/client/.eslintrc.cjs
@@ -1,18 +1,12 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+  extends: ['@nathpaiva/eslint-config-react'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
   },
 }
