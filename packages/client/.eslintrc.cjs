985a6e051d7dbe0a841350c5ebb4ccae91039e07
Nath Paiva <hello@nathpaiva.com.br>
2023-08-18 19:10:10 -0700

    chore: adds new eslint config

--- packages/client/.eslintrc.cjs
+++ packages/client/.eslintrc.cjs
@@ -1,5 +1,8 @@
 module.exports = {
   extends: ['@nathpaiva/eslint-config-react'],
+  rules: {
+    'react/no-unescaped-entities': 0,
+  },
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
