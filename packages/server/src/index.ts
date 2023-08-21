6a6b99b1ce6c7f2caba6ae4adb4d45778f8606a6
Nath Paiva <hello@nathpaiva.com.br>
2023-08-21 10:20:02 -0700

    feat: adds cors config

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -4,14 +4,26 @@ import express from 'express'
 import mongoConnect from './models'
 import { canonicalRouter } from './routes'
 
+// set express to the app
 const app = express()
 
+// set json body parse
 app.use(json())
 
+// set CORS config
+app.use((_, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:2000')
+
+  next()
+})
+
+// create the routes
 app.use('/api', canonicalRouter)
 
+// connect with the DB
 mongoConnect()
 
+// open the connection
 app.listen(3000, () => {
   console.log('server is listening on port 3000')
 })
