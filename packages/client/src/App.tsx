5363f332e1246795405e53c1740c49bb659e6724
Nath Paiva <hello@nathpaiva.com.br>
2023-08-21 09:35:59 -0700

    feat: creates a custom hook

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,11 +1,12 @@
 import { useEffect, useState } from 'react'
+
 import './App.css'
+import { useCsv } from './hooks'
 
 export function App() {
   const [columns, setColumns] = useState<number | null>(null)
-  const [csvParsedDocument, setCsvParsedDocument] = useState<string[] | null>(
-    null,
-  )
+  const { csvParsedDocument, handleOnChangeCsv } = useCsv()
+
   const tableTitle = csvParsedDocument?.length
     ? `Your table has ${columns} columns`
     : ` ${
@@ -19,91 +20,6 @@ export function App() {
 
     console.log('csvDocument =>', csvParsedDocument)
   }, [csvParsedDocument])
-
-  const handleOnChangeCsv = (file: React.ChangeEvent<HTMLInputElement>) => {
-    if (!file.target.files?.length) return
-
-    const fileText = file.target.files[0]
-    const fileReaderInstance = new FileReader()
-
-    fileReaderInstance.addEventListener('load', (event) => {
-      const result = event.target?.result
-
-      if (typeof result === 'string') {
-        const data1 = result?.split('\n').map((row, index) => {
-          if (!index) {
-            return row.split(',')
-          }
-
-          // set if the row is an array
-          let rowIsArray = false
-          // set the position of this row during the normalization
-          let position: number | null = null
-          // create the new row normalized
-          const rowNormalized: string[] = []
-
-          // we can void this by creating an regex to use in the `.split()` 🥲
-          row.split(',').forEach((_item) => {
-            // check if starts a new array
-            const starts = _item.startsWith('"')
-            // check if the end of the array
-            const ends = _item.endsWith('"')
-
-            // if is not an array only add the value to the new rowNormalized
-            if (!starts && !ends && !rowIsArray) {
-              rowNormalized.push(_item)
-            }
-
-            /**
-             * if is starting a new array
-             * set rowIsArray to true
-             * injects the new value to the rowNormalized
-             * set the position of this new item in the rowNormalized
-             */
-            if (starts) {
-              rowIsArray = true
-              rowNormalized.push(_item)
-              position = rowNormalized.length - 1
-            }
-
-            /**
-             * if the rowIsArray and is not the start ot end
-             * concat the content in the rowNormalized in the correct position
-             */
-            if (
-              rowIsArray &&
-              !ends &&
-              !starts &&
-              typeof position === 'number'
-            ) {
-              rowNormalized[position] = `${rowNormalized[position]},${_item}`
-            }
-
-            /**
-             * if is the ends of the array
-             * concat the content in the rowNormalized in the correct position
-             * and reset the rowIsArray
-             * and reset the position to null
-             */
-            if (ends && typeof position === 'number') {
-              rowIsArray = false
-              rowNormalized[position] = `${rowNormalized[position]},${_item}`
-
-              position = null
-            }
-          })
-
-          return rowNormalized
-        })
-
-        const data = data1.flat()
-
-        setCsvParsedDocument(data)
-      }
-    })
-
-    fileReaderInstance.readAsText(fileText)
-  }
 
   const handleHeaderSizerOnChange = (
     event: React.ChangeEvent<HTMLInputElement>,
